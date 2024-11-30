ee01dc95f9a1d6cfb282b09619b7734225bcaf76
ElonArkel <elon.bezerra.120@ufrn.edu.br>
2024-11-29 23:19:58 -0300

    Correção de realocação de memória

---
+++ Aeronave.h
@@ -0,0 +1,14 @@
+#ifndef AERONAVE_H
+#define AERONAVE_H
+
+typedef struct Aeronave
+{
+    char identificador[10];
+    int combustivel;
+    int horario;
+    int tipo;
+    int emergencia;
+    int prioridade;
+} Aeronave;
+
+#endif
